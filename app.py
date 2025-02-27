703ae19d3294bb83c14a3e038e88b56c7c0d5f6e
tarake25 <37196937+tarake25@users.noreply.github.com>
2025-02-27 14:35:51 +0000

    First

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Route for home page
+@app.route('/')
+def index():
+  
+
+
+
+
+
+
+
+  
+  return render_template('index.html')
